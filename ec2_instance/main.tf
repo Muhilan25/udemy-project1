4e881f0a87fca3b5672dbae37edf2c2768a923a5
muhilan <muhilanrangaraj99@gmail.com>
2025-09-08 11:44:57 +0530

    second commit

--- ec2_instance/main.tf
+++ ec2_instance/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "ec2" {
   ami           = var.ami
   instance_type = var.instance_type
-  key_name      = "k8.pem" 
+  key_name      = "k8" 
   tags = {
     Name = var.instance_name
   }
